f4d9e4e88de0a9bf1df818b056d4825db267842f
Matheus <mps.web.tech@gmail.com>
2025-08-25 14:09:50 -0300

    ALPES - Config Auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+// Models
+use App\Models\User;
+
+// Request
+use App\Http\Requests\AuthLoginRequest;
+use App\Http\Requests\GenerateTokenRequest;
+
+//Libs
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+
+    public function generate(GenerateTokenRequest $request)
+    {
+        try {
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+
+            // Remove tokens anteriores e cria um novo token e loga o usuário
+            Auth::login($user);
+
+            $user->tokens()->delete();
+            $token = $user->createToken('api-token')->plainTextToken;
+
+            return response()->json([
+                'user' => $user,
+                'token' => $token
+            ], 201);
+
+        } catch (\Exception $error) {
+            return response()->json([
+                'message' => 'Erro ao criar token.',
+                'error' => $error->getMessage()
+            ], 500);
+        }
+    }
+
+    public function login(AuthLoginRequest $request)
+    {
+
+        try {
+            $user = User::where('email', $request->email)->first();
+
+            if (!$user) {
+                return response()->json([
+                    'message' => 'Usuário não existe.'
+                ], 404);
+            }
+
+            if (!Hash::check($request->password, $user->password)) {
+                return response()->json([
+                    'message' => 'Senha incorreta.'
+                ], 401);
+            }
+
+            $user->tokens()->delete();
+            $token = $user->createToken('api-token')->plainTextToken;
+
+            Auth::login($user);
+
+            return response()->json([
+                'message' => 'Login realizado com sucesso!',
+                'user' => $user,
+                'token' => $token
+            ], 200);
+
+        } catch (\Exception $error) {
+            return response()->json([
+                'message' => 'Erro interno ao tentar realizar login.',
+                'error' => $error->getMessage()
+            ], 500);
+        }
+    }
+}
--- database/migrations/2025_08_23_120755_create_vehicles_table.php
+++ database/migrations/2025_08_23_120755_create_vehicles_table.php
@@ -12,18 +12,27 @@ return new class extends Migration {
     {
         Schema::create('vehicles', function (Blueprint $table) {
             $table->id();
-            $table->json('brands')->nullable();
-            $table->json('models')->nullable();
-            $table->json('versions')->nullable();
-            $table->json('years_model')->nullable();
-            $table->json('years_build')->nullable();
-            $table->json('colors')->nullable();
-            $table->json('fuels')->nullable();
-            $table->json('transmissions')->nullable();
-            $table->json('doors')->nullable();
-            $table->json('units')->nullable();
-            $table->json('km')->nullable(); 
-            $table->json('price')->nullable();
+            $table->string('type');
+            $table->string('brand'); 
+            $table->string('model');
+            $table->string('version')->nullable(); 
+            $table->string('year_model')->nullable();
+            $table->string('year_build')->nullable(); 
+            $table->json('optionals')->nullable();
+            $table->unsignedTinyInteger('doors')->nullable();
+            $table->string('board')->nullable();
+            $table->string('chassi')->nullable();
+            $table->string('transmission')->nullable(); 
+            $table->integer('km')->nullable();
+            $table->text('description')->nullable();
+            $table->boolean('sold')->default(false); 
+            $table->string('category')->nullable();
+            $table->string('url_car')->nullable(); 
+            $table->decimal('old_price', 10, 2)->nullable();
+            $table->decimal('price', 10, 2)->nullable();
+            $table->string('color')->nullable();
+            $table->string('fuel')->nullable();
+            $table->json('fotos')->nullable();
             $table->timestamps();
         });
     }
